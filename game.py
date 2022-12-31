170954a49d01f1e2cc139b07db48bdb3a289abee
vortex73 <nsreekumar6@gmail.com>
2022-12-31 20:24:33 +0530

    embed code finalizedd

--- game.py
+++ game.py
@@ -4,7 +4,7 @@ import firebase_admin
 import discord
 from firebase_admin import credentials
 from firebase_admin import db
-from discord import Game
+from discord import Game, embeds
 from discord.ext.commands import Bot
 BOTPREFIX = "?"
 load_dotenv()
@@ -14,7 +14,7 @@ intents.presences = True
 c = credentials.Certificate(r"key.json")
 firebase_admin.initialize_app(c, {'databaseURL': 'https://discbot-f50a9-default-rtdb.firebaseio.com/'})
 
-
+#client = discord.Client(command_prefix = "!",intents = intents)
 ref = db.reference("/")
 
 users = ref.child('users')
@@ -34,12 +34,12 @@ class chars:
         pass
 
 class user(chars):
-    def __init__(self,typee,power,health,xp,value):
-        self.typee = typee
-        self.power = power
-        self.health = health
-        self.xp = xp 
-        self.value = value
+    def __init__(self,typee,power,health,xp,value,level):
+        super().__init__(typee,power,health,xp,value)
+        self.level = level
+class evil(chars):
+    def __init__(self, typee, power, health, xp, value):
+        super().__init__(typee, power, health, xp, value)
 
 class blow:
 
@@ -58,12 +58,25 @@ class blow:
        # users.push() # test code for firebase
      
     def initialize(self):
-        first_login = True
+        
         @self.bot.event
         async def on_ready():
-            await self.bot.change_presence(activity=Game(name="Fun Games"))
+            await self.bot.change_presence(activity=Game(name="Koala"))
             self.bulk_store()
             print("[*] Connected to Discord as: " + self.bot.user.name)
+        @self.bot.command(name='stats')
+        async def stats(context):
+            player = {"join_server_date": "2020:2:2","xp_points":20}
+           # await context.message.reply(embeds = embed) 
+            #await self.bot.say("So you want stats?")
+            embed = discord.Embed(title = "Brilliant")
+            embed.set_author(name=context.author.display_name,icon_url=context.author.avatar_url)
+            embed.add_field(name="Testing",value="""
+                            Wow
+                            """,inline=True)
+            await context.message.reply(embed=embed)
+                            
+
     def bulk_store(self):
         for i in self.bot.get_all_members():
             self.store(i.id)
