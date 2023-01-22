c8895a8e7a8e43637aa2402ddbf64a7fe0266f0f
vortex73 <nsreekumar6@gmail.com>
2023-01-22 20:52:30 +0530

    integration

--- game.py
+++ game.py
@@ -9,6 +9,7 @@ from datetime import datetime,timedelta
 import json
 import asyncio
 import discord
+from discord.ext import commands
 from discord import app_commands
 from firebase_admin import credentials
 from firebase_admin import db
@@ -19,157 +20,155 @@ load_dotenv()
 intents = discord.Intents.all()
 intents.members = True
 intents.presences = True
-c = credentials.Certificate(r"key.json")
-firebase_admin.initialize_app(c, {'databaseURL': 'https://discbot-f50a9-default-rtdb.firebaseio.com/'})
+
+
+class game(commands.Cog):
+    def __init__(self,bot):
+        self.bot = bot
+                
+        self.c = credentials.Certificate(r"key.json")
+        firebase_admin.initialize_app(self.c, {'databaseURL': 'https://discbot-f50a9-default-rtdb.firebaseio.com/'})
 
 #client = discord.Client(command_prefix = "!",intents = intents)
-ref = db.reference("/")
+        self.ref = db.reference("/")
 
-w = ref.child("users")
-
-class chars:  #Parent Class for general charachter outline
-    def __init__(self,typee,power,health,xp,value):
-        self.typee = typee
-        self.power = power
-        self.health = health
-        self.xp = xp 
-        self.value = value
-    #def get_setting():
-
-    def store_hp(self,id,hp):
-
-        w = ref.child("users")
-        for i in w.get():
-            if int(i)==int(id):
-                temp=w.get()[i]
-                temp["health"]=hp
-                w.update({i:temp})
-    def get_hp(self,id):
-        w = ref.child("users")
-        for i in w.get():
-            if int(i)==int(id):
-                return w.get()[i]["health"]
-    def get_op_hp(self,id):
-        v = ref.child("users").get()[id]["Villains"][self.name]
-        return [v["health"],v["power"]]
-    def str_op_hp(self,id,hp):
-        v = ref.child("users") 
-        temp = v.get()[id]
-        temp["Villains"][self.name]["health"]=hp
-        v.update({id:temp})
-    def combat(self,x,id,atck):
-        oppo_hp ,xp= x.get_op_hp(id)
-        player_health = self.get_hp(id)
-        print(player_health,oppo_hp)
-        if "kick" in atck:
-            x.block()
-            if x.lower_blk==1:
-                return False
-            else:
-                hit_prob = (1-random.random()**self.xp)*self.power
-                x_hit_prob = (1-random.random()**x.xp)*x.power
-                player_health-=x_hit_prob
-                oppo_hp-=hit_prob
-                print(hit_prob,x_hit_prob)
-                print(player_health,oppo_hp)
-                self.store_hp(id,player_health)
-                x.str_op_hp(id,oppo_hp)
-                return [player_health,oppo_hp]
-
-        else:
-            x.block()
-            if x.upper_blk==1:
-                return False
-            else:
-                hit_prob = (1-random.random()**self.xp)*self.power
-                x_hit_prob = (1-random.random()**x.xp)*x.power
-                player_health-=x_hit_prob
-                oppo_hp-=hit_prob
-                
-                print(hit_prob,x_hit_prob)
-                print(player_health,oppo_hp)
-                self.store_hp(id,player_health)
-                x.str_op_hp(id,oppo_hp)
-                return [player_health,oppo_hp]
-            #hit_prob = (1-random.random()**self.xp)*self.power
-            #x_hit_prob = random.random()*x.power
-            #self.health -= x_hit_prob
-            #x.health -= hit_prob
-           # if self.health or x.health == 0:
-            #    break
-
-        #if self.health>x.health:
-        #    return True
-       # else:
-        #    return False
-    
-    @app_commands.command() 
-    async def caution(self,c): # c is context
-        warn = discord.Embed(title = "Warning!")
-        warn.set_thumbnail(url="")
-        warn.add_field(name="",value="")
-        await c.message.reply(embed=warn)
-        #def test(msg,user):
-         #   return user==b.message.author and   
-        #replyy = b.wait_for("reaction_add",check=test)
-    def hunt():
-        pass
-    #async def autoc(interaction:discord.Interaction,msg:str)
-    def abandon(self):
-        self.xp-=10
-        return False
-    def get_level(self):
-        bar = progressBar.filledBar(6,self.level)[1]
-        return bar
-    def buy():
-        pass 
-    def inc_level(self,id):
-        self.level+=1
-        x = w.get()[id]
-        x["level"] = self.level
-        w.update({id:x})
-        with open("story.json") as f:
-            d = json.load(f)
-        a = d[str(self.level)]
-        level = discord.Embed(title="Level Up")
-        level.add_field(name="",value=f"Promoted to level {self.level}")
-       # level.set_thumbnail(url="https://img.freepik.com/premium-vector/game-icon-bonus-level-up-icon-new-level-logo-neon-icon-vector-illustration_100456-4960.jpg?w=740")
-        stry = discord.Embed(title="Next")
-        stry.add_field(name="",value=f"{a['stry']} ")
-      #  stry.set_thumbnail(url=f"{a['img']}")
-        return level,stry
-        
-    # Space to add more player actions
-
-class user(chars):    #Class for player
-    def __init__(self,id,name,typee="Player",power=10,health=100,xp=1,value=None,level=1,money=0,blck=False):
-        super().__init__(typee,power,health,xp,value)
-        self.level = level
-        self.id = id
-        self.name = name
-        self.money = money
-class evil(chars):  #Class for opponents
-    def __init__(self,name,power=20, typee="System wardogs", health=100, xp=1000, value=None,lower_blk=0,upper_blk=0):
-        super().__init__(typee, power, health, xp, value)
-        self.name = name
-    def block(self):
-        self.lower_blk=random.choice([0,1])
-        self.upper_blk=random.choice([0,1])
-            
-
-class blow():
-    def __init__(self,token):
-        self.token = token
-        self.bot = Bot(command_prefix = BOTPREFIX , intents = intents)
+        self.w = self.ref.child("users")
         self.initialize()
-    def run(self):
-        self.bot.run(self.token)
- #       ref = db.reference("/")
-       # # print(ref.get ())
-#        users = ref.child('users')
-       # users.set({
-       # users.push() # test code for firebase
-    
+
+    class chars:  #Parent Class for general charachter outline
+        def __init__(self,typee,power,health,xp,value):
+            self.typee = typee
+            self.power = power
+            self.health = health
+            self.xp = xp 
+            self.value = value
+        #def get_setting():
+
+        def store_hp(self,id,hp):
+
+            w = self.ref.child("users")
+            for i in w.get():
+                if int(i)==int(id):
+                    temp=w.get()[i]
+                    temp["health"]=hp
+                    w.update({i:temp})
+        def get_hp(self,id):
+            w = self.ref.child("users")
+            for i in w.get():
+                if int(i)==int(id):
+                    return w.get()[i]["health"]
+        def get_op_hp(self,id):
+            v = self.ref.child("users").get()[id]["Villains"][self.name]
+            return [v["health"],v["power"]]
+        def str_op_hp(self,id,hp):
+            v = self.ref.child("users") 
+            temp = v.get()[id]
+            temp["Villains"][self.name]["health"]=hp
+            v.update({id:temp})
+        def combat(self,x,id,atck):
+            oppo_hp ,xp= x.get_op_hp(id)
+            player_health = self.get_hp(id)
+            print(player_health,oppo_hp)
+            if "kick" in atck:
+                x.block()
+                if x.lower_blk==1:
+                    return False
+                else:
+                    hit_prob = (1-random.random()**self.xp)*self.power
+                    x_hit_prob = (1-random.random()**x.xp)*x.power
+                    player_health-=x_hit_prob
+                    oppo_hp-=hit_prob
+                    print(hit_prob,x_hit_prob)
+                    print(player_health,oppo_hp)
+                    self.store_hp(id,player_health)
+                    x.str_op_hp(id,oppo_hp)
+                    return [player_health,oppo_hp]
+
+            else:
+                x.block()
+                if x.upper_blk==1:
+                    return False
+                else:
+                    hit_prob = (1-random.random()**self.xp)*self.power
+                    x_hit_prob = (1-random.random()**x.xp)*x.power
+                    player_health-=x_hit_prob
+                    oppo_hp-=hit_prob
+                    
+                    print(hit_prob,x_hit_prob)
+                    print(player_health,oppo_hp)
+                    self.store_hp(id,player_health)
+                    x.str_op_hp(id,oppo_hp)
+                    return [player_health,oppo_hp]
+                #hit_prob = (1-random.random()**self.xp)*self.power
+                #x_hit_prob = random.random()*x.power
+                #self.health -= x_hit_prob
+                #x.health -= hit_prob
+               # if self.health or x.health == 0:
+                #    break
+
+            #if self.health>x.health:
+            #    return True
+           # else:
+            #    return False
+        
+        @app_commands.command() 
+        async def caution(self,c): # c is context
+            warn = discord.Embed(title = "Warning!")
+            warn.set_thumbnail(url="")
+            warn.add_field(name="",value="")
+            await c.message.reply(embed=warn)
+            #def test(msg,user):
+             #   return user==b.message.author and   
+            #replyy = b.wait_for("reaction_add",check=test)
+        def hunt():
+            pass
+        #async def autoc(interaction:discord.Interaction,msg:str)
+        def abandon(self):
+            self.xp-=10
+            return False
+        def get_level(self):
+            level = self.w.get()[str(self.id)]["level"]
+            bar = progressBar.filledBar(6,level)[0]
+            return bar
+        def buy():
+            pass 
+        def inc_level(self,id):
+            
+            x = self.w.get()[id]
+            x["level"] += 1
+            self.w.update({id:x})
+            with open("story.json") as f:
+                d = json.load(f)
+            a = d[str(x["level"])]
+            level = discord.Embed(title="Level Up")
+            level.add_field(name="",value=f"Promoted to level {x['level']}")
+            level.set_thumbnail(url="https://img.freepik.com/premium-vector/game-icon-bonus-level-up-icon-new-level-logo-neon-icon-vector-illustration_100456-4960.jpg?w=740")
+            stry = discord.Embed(title="Next")
+            stry.add_field(name="",value=f"{a['stry']} ")
+            stry.set_image(url=f"{a['img']}")
+            return level,stry
+            
+        # Space to add more player actions
+     
+    class user(chars):    #Class for player
+        def __init__(self,id,name,typee="Player",power=10,health=100,xp=1,value=None,level=1,money=0,blck=False):
+            super().__init__(typee,power,health,xp,value)
+            self.level = level
+            self.id = id
+            self.name = name
+            self.money = money
+            self.w = self.ref.child("users")
+            self.ref = db.reference("/")
+    class evil(chars):  #Class for opponents
+        def __init__(self,name,power=20, typee="System wardogs", health=100, xp=1000, value=None,lower_blk=0,upper_blk=0):
+            super().__init__(typee, power, health, xp, value)
+            self.name = name
+        def block(self):
+            self.lower_blk=random.choice([0,1])
+            self.upper_blk=random.choice([0,1])
+                
+
+                
     def initialize(self):
         
         @self.bot.event
@@ -199,7 +198,7 @@ class blow():
                 l.append(app_commands.Choice(name=x,value=x))
             return l
         async def buy_cmd(i:discord.Interaction,current:str)->list[app_commands.Choice[str]]:
-            w = ref.child("weapons")
+            w = self.ref.child("weapons")
             for i in w.get().values():   # i={weapons:[{guns:{}}]}
                 for j in i.values():  # j=[{guns:{},melee:{}}] list of categories\
                     cat = []
@@ -208,7 +207,7 @@ class blow():
                             cat.append(app_commands.Choice(name=l,value=l)) 
             return cat 
         async def buy_item(i:discord.Interaction,current:str)->list[app_commands.Choice[str]]:
-            w = ref.child("weapons")
+            w = self.ref.child("weapons")
             for i in w.get().values():   # i={weapons:[{guns:{}}]}
                 print(f"current = {current}") 
                 for j in i.values():  # j=[{guns:{},melee:{}}] list of categories\
@@ -224,43 +223,55 @@ class blow():
 
             
               
-            
+        @self.bot.tree.command(name="story",description="The story so far")
+        async def story(i:discord.Interaction):
+            await i.response.defer() 
+            with open("story.json") as f:
+                d = json.load(f)
+            stry = discord.Embed(title="Story")
+            for x in d:
+                if int(x)<=int(self.w.get()[str(i.user.id)]["level"])+1:
+                    stry.add_field(name="",value=f"{d[x]['stry']}")
+            stry.set_image(url="https://rukminim1.flixcart.com/image/850/500/kuyf8nk0/poster/t/3/6/medium-the-punisher-frank-castle-marvel-comics-matte-finish-original-imag7ys46cfaurh6.jpeg?q=90")
+            await i.followup.send(embed=stry)
+                    
+
             
         @self.bot.tree.command(name="stats",description="Player stats.")
-        async def stats(context):
+        async def stats(i):
            # player = {"join_server_date": "2020:2:2","xp_points":20}
            # await context.message.reply(embeds = embed) 
             #await self.bot.say("So you want stats?")
-
-            embed = discord.Embed(title = "Brilliant")
-            embed.set_author(name=context.author.display_name,icon_url=context.author.avatar_url)
-            embed.add_field(name="Testing",value="""
+            await i.response.defer() 
+            embed = discord.Embed(title = "Player Stats")
+            embed.set_author(name=i.user.display_name,icon_url=i.user.display_avatar)
+            embed.add_field(name="",value="""
                             Wow
                             """,inline=True)
 
             embed = discord.Embed(title = "")
-            embed.set_author(name=context.author.display_name,icon_url=context.author.avatar_url)
-            u = ref.child("user/player")
+            embed.set_author(name=i.user.id,icon_url=i.user.display_avatar)
+            u = self.ref.child("user/player")
 
             embed = discord.Embed(title = "Player Stats")
-            embed.set_author(name=context.author.display_name)
-            embed.set_thumbnail(url=context.author.avatar_url)
+            embed.set_author(name=i.user.display_name)
+            embed.set_thumbnail(url=i.user.display_avatar)
             embed.add_field(name="",value="""<Write something here>""",inline=True)
 
-            await context.message.reply(embed=embed)
+            await i.followup.send(embed=embed)
 
         @self.bot.tree.command(name="attack",description="Launch an attack on the nearest boss")
         @app_commands.autocomplete(attack=atk_cmd)
         #@app_commands.autocomplete(atk=auto_cmd)
         async def attack(i :discord.InteractionResponse,attack:str)->list[app_commands.Choice[str]]:
-            u = ref.child("users")
+            u = self.ref.child("users")
             t = u.get()[str(i.user.id)]["cooldown"]
             if str(datetime.now())>t:
 
-                player = user(i.user.id,i.user.display_name,power=10)
+                player = self.user(i.user.id,i.user.display_name,power=10)
                 v = get_villain(player)
-                x = ref.child("users").get()[str(i.user.id)]["Villains"][v]
-                oppo = evil(name=v,health=x["health"],power=x["power"])
+                x = self.ref.child("users").get()[str(i.user.id)]["Villains"][v]
+                oppo = self.evil(name=v,health=x["health"],power=x["power"])
                 await i.response.defer()
                 fn=player.combat(oppo,str(i.user.id),attack)
                 if fn:
@@ -287,7 +298,7 @@ class blow():
         @self.bot.tree.command(name="weapon",description="Lists out the weapons in the Store.")
 
         async def weapon(interaction):
-            w = ref.child("weapons")
+            w = self.ref.child("weapons")
             #g = w.get()
             #wname = list(g.values())
             shop = discord.Embed(title="Shop")
@@ -306,7 +317,7 @@ class blow():
 
         @self.bot.tree.command(name="level",description="know your level")
         async def lvl(context):
-            client = user(context.user.id,context.user.display_name)
+            client = self.user(context.user.id,context.user.display_name)
             lvl = discord.Embed(title="Your Level:")
             lvl.add_field(name="",value=client.get_level())
             await context.response.send_message(embed=lvl)
@@ -318,8 +329,8 @@ class blow():
              purchase = discord.Embed(title = "Your Purchase")
              purchase.add_field(name="category",value=category)
              purchase.add_field(name="Item",value=item)
-             purchase.add_field(name="Cost",value=get(item)[0])
-             purchase.add_field(name="Damage",value=get(item)[1])
+             purchase.add_field(name="Cost",value=self.get(item)[0])
+             purchase.add_field(name="Damage",value=self.get(item)[1])
              await i.response.send_message(embed=purchase,ephemeral=True)
         
        # @app_commands.autocomplete(item=buy_item)
@@ -345,10 +356,10 @@ class blow():
         @self.bot.tree.command(name="train",description="Train and gain more xp")
         @app_commands.autocomplete(atck=atk_cmd)
         async def hunt(i:discord.Interaction,atck:str)->list[app_commands.Choice[str]]:
-            player = user(i.user.id,i.user.display_name,power=10)
-            w = ref.child("users").get()
+            player = self.user(i.user.id,i.user.display_name,power=10)
+            w = self.ref.child("users").get()
             t = w[str(player.id)]["Villains"]["trainer"]
-            oppo = evil(name="trainer",health=t["health"],power=t["power"],xp=t["xp"])
+            oppo = self.evil(name="trainer",health=t["health"],power=t["power"],xp=t["xp"])
             await i.response.defer()
             fn = player.combat(oppo,str(player.id),atck)
             if fn:
@@ -362,13 +373,13 @@ class blow():
                 await i.followup.send("Your attack was blocked")
     def bulk_store(self):      # Function to store all the members in the server
         for i in self.bot.get_all_members():
-            if ref.child("users").get()==None or i.id not in ref.child('users').get() :
-                x=user(i.id,i.name)
+            if self.ref.child("users").get()==None or i.id not in self.ref.child('users').get() :
+                x=self.user(i.id,i.name)
                 self.store(x)
 
     def store(self,x):         # Function to push given datapoint into DB
             
-            users = ref.child('users')
+            users = self.ref.child('users')
             users.update({
                 x.id:{"typee":x.typee,
                        "Name" : x.name,
@@ -389,15 +400,15 @@ class blow():
 }
                     }})
     
-    file = open(r"weapons.json","r")
-    i=json.load(file)
-    armoury = ref.child('weapons')
-    armoury.push(i)
+            file = open(r"weapons.json","r")
+            i=json.load(file)
+            armoury = self.ref.child('weapons')
+            armoury.push(i)
 
         
-i=blow(os.getenv("token"))
+#i=blow(os.getenv("token"))
 
 
 #i.bot.load_extension(r"message.py") # TBD in final steps.
-print(i.run())
+#print(i.run())
 
--- message.py
+++ message.py
@@ -1,34 +1,7 @@
-import os
-from dotenv import load_dotenv
 import discord
 from discord.ext import commands, tasks
 import scrapetube
 
-
-load_dotenv()
-intents = discord.Intents.all()
-intents.members = True
-intents.presences = True     
-
-bot = commands.Bot(command_prefix=".", intents=intents)
-
-
-@bot.event
-async def on_ready():
-    print("I am online")   #For bot to tell when it's online
-
-@bot.event
-async def on_member_join(member):
-    await Bot.get_channel(1044294754818609216).send("Welcome {}".format(member.mention)) # Welcome msg 
-
-@bot.command(name="clear")
-async def clear(msgs,amount=5):
-    await msgs.channel.purge(limit=amount+1)
-    
-
-@bot.command(name="nuke")
-async def clear(msgs):
-    await msgs.channel.purge()
 
 class Youtube(commands.Cog):
     def __init__(self,Bot):
@@ -39,47 +12,66 @@ class Youtube(commands.Cog):
 
                 }
         self.videos = {}
+        self.initialize()
 
-    @commands.Cog.listener()
-    async def on_ready(self):
-        self.check.start()
+    def initialize(self): 
+        @commands.Cog.listener()
+        async def on_ready(self):
+            print("I am online")   #For bot to tell when it's online
+            await self.Bot.tree.sync()
+            self.check.start()
+        @commands.Cog.listener()
+        async def on_member_join(self,member):
+            await self.Bot.get_channel(1044294754818609216).send("Welcome {}".format(member.mention)) # Welcome msg 
+        async def amt(i:discord.Interaction,current:str)->list[discord.app_commands.Choice[str]]:
+            l=[]
+            for i in [1,5,10,20,30,50]:
+                l.append(discord.app_commands.Choice(name=i,value=i))
+            return l
+        @self.Bot.tree.command(name="clear")
+        @discord.app_commands.autocomplete(amount=amt)
+        async def clear(i:discord.Interaction,amount:int)->list[discord.app_commands.Choice[str]]:
+            await i.channel.purge(limit=amount+1)
+            
 
-    @tasks.loop(seconds=60)
-    async def check(self):
-        discord_channel = self.Bot.get_channel(1044294754818609216)
+        @self.Bot.tree.command(name="nuke")
+        async def clear(i:discord.Interaction):
+            await i.response.send_message("Deleting...")
+            await i.channel.purge()
 
-        for channel_name in self.channels:
-            videos = scrapetube.get_channel(channel_url = self.channels[channel_name])
-            video_ids = [video["videoId"] for video in videos]
+        
+        @tasks.loop(seconds=60)
+        async def check(self):
+            discord_channel = self.Bot.get_channel(1044294754818609216)
 
-            if self.check.current_loop == 0 :
+            for channel_name in self.channels:
+                videos = scrapetube.get_channel(channel_url = self.channels[channel_name])
+                video_ids = [video["videoId"] for video in videos]
+
+                if self.check.current_loop == 0 :
+                    self.videos[channel_name] = video_ids
+                    continue
+
+                for video_id in video_ids:
+                    if video_id not in self.videos[channel_name]:
+                        url = f"https://youtube.com/{video_id}"
+                        await discord_channel.send(f"{channel_name} has uploaded a new video\n{url}")
+
+
                 self.videos[channel_name] = video_ids
-                continue
-
-            for video_id in video_ids:
-                if video_id not in self.videos[channel_name]:
-                    url = f"https://youtube.com/{video_id}"
-                    await discord_channel.send(f"{channel_name} has uploaded a new video\n{url}")
-
-
-            self.videos[channel_name] = video_ids
-
-@bot.event
-async def on_ready():
-    print("I'm on")
-    await bot.add_cog(Youtube(bot))
 
 
 
-@bot.command(name="start-yt")  #function to tell the bot to start otifying
-async def start_notifications(notify):
-    Youtube(bot).check.start()
-    await notify.send("Now notifying")
 
-@bot.command(name="stop-yt")  #function to tell the bot to stop notifying
-async def stop_notifications(notify):
-    Youtube(bot).check.stop()
-    await notify.send("Stopped notifying")
+        @self.Bot.tree.command(name="start-yt")  #function to tell the bot to start otifying
+        async def start_notifications(i:discord.Interaction):
+            Youtube(self.Bot).check.start()
+            await i.response.send_message("Now notifying")
 
-bot.run(os.getenv("token"))
+        @self.Bot.tree.command(name="stop-yt")  #function to tell the bot to stop notifying
+        async def stop_notifications(i:discord.Interaction):
+            Youtube(self.Bot).check.stop()
+            await i.response.send_message("Stopped notifying")
+
+#bot.run(os.getenv("token"))
 
